c8ced328f01176655e0e09f56c187628fc06cc2e
fishka11 <r.piasnik@gmail.com>
2019-05-13 22:14:52 +0200

    Add onAttributesChangeRenderer decorator

--- src/common/decorator.js
+++ src/common/decorator.js
@@ -1,8 +1,28 @@
+/* eslint-disable no-param-reassign */
 export function markdownRenderer(targetConstructor) {
-  // eslint-disable-next-line no-param-reassign
   targetConstructor.prototype.renderMarkdown = content => (`
     <mark-down>
        ${content}
     </mark-down>
   `);
+}
+
+export function onAttributesChangeRenderer(attributes) {
+  return function decorator(targetConstructor) {
+    Object.defineProperty(targetConstructor, 'observedAttributes', {
+      get() {
+        return attributes;
+      }
+    });
+    const proto = targetConstructor.prototype;
+    proto.attributeChangedCallback = function callback(name, oldValue, newValue) {
+      if (oldValue !== newValue) {
+        this.render();
+      }
+    };
+    proto.init = function init() {
+      this.attachShadow({ mode: 'open' });
+      this.render();
+    };
+  };
 }
